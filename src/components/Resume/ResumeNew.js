2d872386fa358db0216bc49970373b4100a2b5c5
fsquaglia <fernandosquaglia@gmail.com>
2024-03-24 19:58:50 -0300

    cambiado pdf resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/FOS_CV.pdf";
+import pdf from "../../Assets/../Assets/FOS_CV_2024_FSD.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
